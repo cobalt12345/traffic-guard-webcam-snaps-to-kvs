131b420ffe18aeea4d1e552e630cfebd381148de
Denis Talochkin <vgtu@rambler.ru>
2021-05-19 20:25:19 +0300

    The most promising variant.

--- WebcamSnaps2KvsFunction/src/main/java/den/tal/traffic/guard/WebcamStreamProcessor.java
+++ WebcamSnaps2KvsFunction/src/main/java/den/tal/traffic/guard/WebcamStreamProcessor.java
@@ -1,41 +1,38 @@
 package den.tal.traffic.guard;
 
 import com.amazonaws.services.kinesisvideo.AmazonKinesisVideoPutMedia;
+import com.amazonaws.services.kinesisvideo.PutMediaAckResponseHandler;
+import com.amazonaws.services.kinesisvideo.model.AckEvent;
+import com.amazonaws.services.kinesisvideo.model.FragmentTimecodeType;
+import com.amazonaws.services.kinesisvideo.model.PutMediaRequest;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
 import com.amazonaws.util.Base64;
-import com.fasterxml.jackson.databind.util.ByteBufferBackedInputStream;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import den.tal.traffic.guard.json.BodyPayload;
 import den.tal.traffic.guard.kvs.utils.Utils;
 import lombok.extern.slf4j.Slf4j;
-import org.jcodec.api.SequenceEncoder;
 import org.jcodec.codecs.h264.H264Encoder;
-import org.jcodec.codecs.png.PNGEncoder;
-import org.jcodec.common.Codec;
-import org.jcodec.common.MuxerTrack;
 import org.jcodec.common.VideoCodecMeta;
 import org.jcodec.common.VideoEncoder;
 import org.jcodec.common.io.ByteBufferSeekableByteChannel;
-import org.jcodec.common.io.FileChannelWrapper;
 import org.jcodec.common.model.*;
-import org.jcodec.containers.mkv.boxes.EbmlMaster;
 import org.jcodec.containers.mkv.muxer.MKVMuxer;
 import org.jcodec.containers.mkv.muxer.MKVMuxerTrack;
 import org.jcodec.scale.AWTUtil;
-import org.jcodec.scale.RgbToYuv420p;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
-import java.io.*;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import java.nio.ByteBuffer;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Date;
-import java.util.function.Consumer;
-
-import static den.tal.traffic.guard.kvs.utils.Utils.CODEC_PRIVATE_DATA_640x480_25;
+import java.util.concurrent.CountDownLatch;
 
 /**
  * Current lambda function processes JPEG images, converts them to h264 frames and sends to AWS.
@@ -46,71 +43,116 @@ import static den.tal.traffic.guard.kvs.utils.Utils.CODEC_PRIVATE_DATA_640x480_2
 public class WebcamStreamProcessor implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent>
 {
     private final Gson gson = new GsonBuilder().setPrettyPrinting().create();
-    private int processedFrameNum = 0;
     private H264Encoder encoder = H264Encoder.createH264Encoder();
 
     @Override
     public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent request, Context context) {
-        AmazonKinesisVideoPutMedia kinesisVideoPutMediaClient = Utils.getKvsPutMediaClient(Utils.getRegion(),
-                Utils.getKvsName());
 
-        processImages(request.getBody(), Utils.getImageFormatPrefix(), Utils.getProcessor(kinesisVideoPutMediaClient));
+        try (ByteBufferSeekableByteChannel outChannel =
+                     convertImages(request.getBody(), Utils.getImageFormatPrefix(),                false);
+
+             ByteArrayInputStream is =
+                new ByteArrayInputStream(outChannel.getContents().array());
+
+             AmazonKinesisVideoPutMedia putMediaClient = Utils.getKvsPutMediaClient(Utils.getRegion(),
+                     Utils.getKvsName())) {
+
+            CountDownLatch latch = new CountDownLatch(1);
+            putMediaClient.putMedia(new PutMediaRequest().withStreamName(Utils.getKvsName())
+                    .withFragmentTimecodeType(FragmentTimecodeType.RELATIVE)
+                    .withProducerStartTimestamp(new Date())
+                    .withPayload(is), new PutMediaAckResponseHandler() {
+
+                @Override
+                public void onAckEvent(AckEvent event) {
+                    log.debug("Fragment ack. {}", event);
+                }
+
+                @Override
+                public void onFailure(Throwable t) {
+                    log.error("Fragment send error.", t);
+                    latch.countDown();
+                }
+
+                @Override
+                public void onComplete() {
+                    log.debug("Fragment sent.");
+                    latch.countDown();
+                }
+            });
+
+            latch.await();
+
+        } catch (IOException | InterruptedException ioex) {
+            log.error("Could not convert images to Mkv", ioex);
+
+            throw new RuntimeException(ioex);
+        }
 
         return Utils.getResponse(200, "OK");
     }
 
-    void processImages(String jsonBody, String imageFormat, Consumer<ByteBuffer> processor) {
+    ByteBufferSeekableByteChannel convertImages(String jsonBody, String imageFormat, boolean storeImagesAsFiles)
+            throws IOException {
+
         if (jsonBody != null && !jsonBody.isEmpty()) {
             BodyPayload payload = gson.fromJson(jsonBody, BodyPayload.class);
             Picture[] pictures = new Picture[payload.getFrames().length];
             int bufferSize = 0;
             Rational rational = Rational.R(payload.getFrames().length, 1);
-            for (int i = 0; i < payload.getFrames().length; ++i, ++processedFrameNum) {
-                log.debug("Process image. Batch ordinal num: {}. Absolute num: {}",
-                        i, processedFrameNum);
-
+            for (int i = 0; i < payload.getFrames().length; ++i) {
+                log.debug("Process image. Batch ordinal num: {}.", i);
                 final String image64base = payload.getFrames()[i];
                 BufferedImage bufferedImage = convertToImage(normalize(image64base, imageFormat));
                 Picture picture = AWTUtil.fromBufferedImage(bufferedImage, encoder.getSupportedColorSpaces()[0]);
+
+                if (storeImagesAsFiles) {
+                    Path pngFile = Files.createTempFile("img", ".png");
+                    log.debug("Save PNG {}", pngFile.toAbsolutePath());
+                    AWTUtil.savePicture(picture, "PNG", pngFile.toFile());
+                }
                 pictures[i] = picture;
                 bufferSize += encoder.estimateBufferSize(pictures[i]);
             }
+            ByteBuffer allocatedBuffer = ByteBuffer.allocate(bufferSize);
+            ByteBufferSeekableByteChannel outChannel = new ByteBufferSeekableByteChannel(
+                    allocatedBuffer, bufferSize);
 
-            try {
-                ByteBuffer allocatedBuffer = ByteBuffer.allocate(bufferSize);
-                MKVMuxer muxer = new MKVMuxer(new ByteBufferSeekableByteChannel (
-                        allocatedBuffer, pictures.length));
+            MKVMuxer muxer = new MKVMuxer(outChannel);
+            MKVMuxerTrack videoTrack = null;
+            for (int frameNum = 0; frameNum < pictures.length; frameNum++) {
+                var pic = pictures[frameNum];
+                if (videoTrack == null) {
+                    videoTrack = muxer.createVideoTrack(VideoCodecMeta.createVideoCodecMeta(
+                    "mkv", ByteBuffer.wrap(Utils.CODEC_PRIVATE_DATA_640x480_25),
+                            new Size(pic.getWidth(), pic.getHeight()), Rational.R(pictures.length, 1)),
+                            "V_MPEG4/ISO/AVC");
 
-                MuxerTrack videoTrack = null;
-
-                for (int frameNum  = 0; frameNum < pictures.length; frameNum++) {
-                    var pic = pictures[frameNum];
-                    if (videoTrack == null) {
-                        videoTrack = muxer.addVideoTrack(Codec.H264, VideoCodecMeta.createSimpleVideoCodecMeta(
-                                new Size(pic.getWidth(), pic.getHeight()), encoder.getSupportedColorSpaces()[0]));
 
 //                        videoTrack = muxer.addVideoTrack(Codec.H264, VideoCodecMeta.createVideoCodecMeta(
 //                    "X264", ByteBuffer.wrap(CODEC_PRIVATE_DATA_640x480_25),
 //                                new Size(pic.getWidth(), pic.getHeight()), Rational.R(25, 1)));
-                    }
-                    int pictureBufferSize = encoder.estimateBufferSize(pic);
-                    ByteBuffer pictureBuffer = ByteBuffer.allocate(pictureBufferSize);
-                    VideoEncoder.EncodedFrame encodedFrame = encoder.encodeFrame(pic, pictureBuffer);
-
-                    videoTrack.addFrame(Packet.createPacket(encodedFrame.getData(), new Date().getTime(),
-                            rational.getNum(), 1000 / pictures.length, frameNum,
-                            frameNum == pictures.length - 1 ?
-                                    Packet.FrameType.KEY : Packet.FrameType.INTER, TapeTimecode.ZERO_TAPE_TIMECODE));
                 }
-                muxer.finish();
-                processor.accept(allocatedBuffer);
-            } catch (Exception e) {
-                log.error("Payload images cannot be converted into the sequence.", e);
+                int pictureBufferSize = encoder.estimateBufferSize(pic);
+                ByteBuffer pictureBuffer = ByteBuffer.allocate(pictureBufferSize);
+                VideoEncoder.EncodedFrame encodedFrame = encoder.encodeFrame(pic, pictureBuffer);
+                Packet packet = Packet.createPacket(encodedFrame.getData(),
+                        new Date().getTime(),
+                        10000,
+                        200_000_000,
+                        frameNum,
+                        encodedFrame.isKeyFrame() ? Packet.FrameType.KEY : Packet.FrameType.INTER,
+                        TapeTimecode.tapeTimecode(frameNum, false, pictures.length));
+
+                videoTrack.addFrame(packet);
             }
+            muxer.finish();
 
-
+            return outChannel;
         } else {
             log.debug("Method body is empty. No images for processing.");
+
+            return null;
         }
     }
 
--- WebcamSnaps2KvsFunction/src/test/java/den/tal/traffic/guard/WebcamStreamProcessorTest.java
+++ WebcamSnaps2KvsFunction/src/test/java/den/tal/traffic/guard/WebcamStreamProcessorTest.java
@@ -4,14 +4,22 @@ import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.amazonaws.util.IOUtils;
 import com.fasterxml.jackson.databind.util.ByteBufferBackedInputStream;
 import lombok.extern.slf4j.Slf4j;
+import org.jcodec.api.transcode.*;
+import org.jcodec.api.transcode.filters.ScaleFilter;
+import org.jcodec.common.Codec;
+import org.jcodec.common.Format;
+import org.jcodec.common.Tuple;
+import org.jcodec.common.io.ByteBufferSeekableByteChannel;
+import org.jcodec.common.io.FileChannelWrapper;
+import org.jcodec.common.io.NIOUtils;
 import org.junit.Test;
 
-import java.io.*;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.nio.ByteBuffer;
-import java.nio.channels.FileChannel;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.nio.file.StandardCopyOption;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -19,6 +27,27 @@ import java.util.stream.Collectors;
 public class WebcamStreamProcessorTest {
 
     private static final String REQUEST_PAYLOAD = "request_payload.json";
+
+//    @Test
+    public void webMovieAndMKVStreamingMuxerTest() throws IOException {
+        Transcoder.TranscoderBuilder builder = Transcoder.newTranscoder();
+        String sourceName = "C:\\Users\\User\\AppData\\Local\\Temp\\img%d.png";
+
+        String destName = Files.createTempFile("kvs", ".mkv").toAbsolutePath().toString();
+        Source source = new SourceImpl(sourceName, Format.IMG, Tuple.triple(0, 0, Codec.PNG), null);
+        source.setOption(Options.INTERLACED, false);
+        builder.addSource(source);
+        builder.setSeekFrames(0, 0).setMaxFrames(0, 25);
+
+        Sink sink = new SinkImpl(destName, Format.MKV, Codec.H264, null);
+        builder.addSink(sink);
+        builder.addFilter(0, new ScaleFilter(640, 480));
+        builder.setVideoMapping(0, 0, true);
+        Transcoder transcoder = builder.create();
+        transcoder.transcode();
+
+
+    }
 
     @Test
     public void handleRequestTest() throws Exception {
@@ -28,7 +57,14 @@ public class WebcamStreamProcessorTest {
         payload.forEach(System.out::println);
         String strPayload = payload.stream().collect(Collectors.joining());
 
-        new WebcamStreamProcessor().processImages(strPayload, "data:image/png;base64,", this::createMkvFile);
+        ByteBufferSeekableByteChannel outChannel = new WebcamStreamProcessor().convertImages(strPayload,
+                "data:image/png;base64,", true);
+
+        Path mkvFile = Files.createTempFile("", ".mkv");
+        log.debug("Temp Mkv file: {}", mkvFile.toAbsolutePath());
+        try (outChannel; FileChannelWrapper fileChannel = NIOUtils.writableChannel(mkvFile.toFile())) {
+            fileChannel.write(outChannel.getContents());
+        }
     }
 
     public void createMkvFile(ByteBuffer fragment) {
